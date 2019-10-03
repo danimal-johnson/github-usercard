d348510abef7c5d769578fdec532ae2109d9b102
Dan Johnson <danimal.johnson@gmail.com>
2019-10-03 15:27:20 -0700

    Basic functionality working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,56 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
+/////////////// Uncomment this again for final testintg /////////////////
+// let myself = null;
+// axios
+//   .get ("https://api.github.com/users/danimal-johnson")
+//   .then(response => {
+//     console.log(response);
+//     // Process the data
+//     myself = response.data;
+//   })
+//   .catch(error => {
+//     console.error("The data was not returned");
+//   });
+
+////////// This is just for temporary lookup to save on API calls //////////
+  const me = {
+    "login": "danimal-johnson",
+    "id": 20260285,
+    "node_id": "MDQ6VXNlcjIwMjYwMjg1",
+    "avatar_url": "https://avatars0.githubusercontent.com/u/20260285?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/danimal-johnson",
+    "html_url": "https://github.com/danimal-johnson",
+    "followers_url": "https://api.github.com/users/danimal-johnson/followers",
+    "following_url": "https://api.github.com/users/danimal-johnson/following{/other_user}",
+    "gists_url": "https://api.github.com/users/danimal-johnson/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/danimal-johnson/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/danimal-johnson/subscriptions",
+    "organizations_url": "https://api.github.com/users/danimal-johnson/orgs",
+    "repos_url": "https://api.github.com/users/danimal-johnson/repos",
+    "events_url": "https://api.github.com/users/danimal-johnson/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/danimal-johnson/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Dan Johnson",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "public_repos": 22,
+    "public_gists": 0,
+    "followers": 1,
+    "following": 1,
+    "created_at": "2016-07-02T21:09:34Z",
+    "updated_at": "2019-09-25T07:08:14Z"
+  }
+  
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -10,9 +59,13 @@
    Skip to Step 3.
 */
 
+
+
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+
+const cardLocation = document.querySelector(".cards");
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -24,10 +77,35 @@
           user, and adding that card to the DOM.
 */
 
+////////// Temporary local lookup for API call ///////////////
+myFollowers = [
+  {
+    "login": "joenelsong",
+    "id": 7134689,
+    "node_id": "MDQ6VXNlcjcxMzQ2ODk=",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/7134689?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/joenelsong",
+    "html_url": "https://github.com/joenelsong",
+    "followers_url": "https://api.github.com/users/joenelsong/followers",
+    "following_url": "https://api.github.com/users/joenelsong/following{/other_user}",
+    "gists_url": "https://api.github.com/users/joenelsong/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/joenelsong/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/joenelsong/subscriptions",
+    "organizations_url": "https://api.github.com/users/joenelsong/orgs",
+    "repos_url": "https://api.github.com/users/joenelsong/repos",
+    "events_url": "https://api.github.com/users/joenelsong/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/joenelsong/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+]
+//////////// Put the actual API call into this array. ////////////////
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+          Using DOM methods and properties, create a component that will return
+          the following DOM element:
 
 <div class="card">
   <img src={image url of user} />
@@ -45,6 +123,82 @@ const followersArray = [];
 </div>
 
 */
+
+const createCard = function (person) {
+
+  console.log ("Making a card");
+  // Create the card fields
+  const newCard       = document.createElement("div");  // L1
+  const newImage      = document.createElement("img");    // L2
+  const newCardInfo   = document.createElement("div");    // L2
+  const newName       = document.createElement("h3");       // L3
+  const newUserName   = document.createElement("p");        // L3
+  const newLocation   = document.createElement("p");        // L3
+  const newProfile    = document.createElement("p");        // L3
+  const newGHAddr     = document.createElement("a");          // L4
+  const newFlwrCount  = document.createElement("p");        // L3
+  const newFlwngCount = document.createElement("p");        // L3
+  const newBio        = document.createElement("p");        // L3
+
+  // Add class names to the fields
+  newCard.classList.add("card");
+  newCardInfo.classList.add("card-info");
+  newName.classList.add("name");
+  newUserName.classList.add("username");
+      
+  // Add structure to the object (Follow the levels above)
+  newCard.appendChild(newImage);
+  newCard.appendChild(newCardInfo);
+
+  newCardInfo.appendChild(newName);
+  newCardInfo.appendChild(newUserName);
+  newCardInfo.appendChild(newLocation);
+  newCardInfo.appendChild(newProfile);
+  newCardInfo.appendChild(newFlwrCount);
+  newCardInfo.appendChild(newFlwngCount);
+  newCardInfo.appendChild(newBio);
+
+  newProfile.appendChild(newGHAddr);
+
+  // Populate all the values
+  newImage.src              = person.avatar_url;
+  newName.textContent       = person.name;
+  newUserName.textContent   = person.login;
+  newLocation.textContent   = `Location: ${person.location}`;
+  // newProfile.textContent    = "Profile: ";
+  let userUrl               = `https://github.com/${person.login}/`;
+  newGHAddr.textContent     = userUrl;
+  newGHAddr.href            = userUrl;
+  newFlwrCount.textContent  = `Followers: ${person.followers}`;
+  newFlwngCount.textContent = `Following: ${person.following}`;
+  newBio.textContent        = `Bio: ${person.bio}`;
+
+  console.log("Got a guy named " + person.name);
+
+  return newCard;
+}
+/*
+
+  <img src={image url of user} />
+  <div class="card-info">
+    <p>Location: {users location}</p>
+    <p>Profile:  
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+  </div>
+
+*/
+
+// Add Me
+let myCard = createCard (me);
+cardLocation.appendChild(myCard);
+
+// Add my real followers
+let fCard = createCard (myFollowers[0]);
+cardLocation.appendChild(fCard);
+
+// Add some fake followers to pad the mix
+
 
 /* List of LS Instructors Github username's: 
   tetondan
